3b19414681a301e4c325ffdf42f831474988c7c2
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2023-11-11 02:47:20 +0100

    Updates to NAVBAR

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorizeService } from '../../api-authorization/authorize.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
+
+  constructor(private authorizeService: AuthorizeService) { }
+
+  isLoggedIn: boolean = false;
+
+
+  ngOnInit() {
+    this.authorizeService.isAuthenticated().subscribe(
+      (authStatus) => {
+        this.isLoggedIn = authStatus;
+      },
+      (error) => {
+        console.error('Error checking authentication status', error);
+      }
+    );
+  }
+
+
   isExpanded = false;
   //Denne funksjonaliteten kan fjernes siden vi ikke trenger å skru nav baren av og på
   collapse() {
