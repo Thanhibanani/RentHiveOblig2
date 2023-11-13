7549bb704e31dc454fec2611e74f78fa58c64a8e
Thanh Nguyen <71760329+Thanhibanani@users.noreply.github.com>
2023-11-13 14:58:05 +0100

    ok

--- ClientApp/src/app/listingdetails/listingdetails.component.ts
+++ ClientApp/src/app/listingdetails/listingdetails.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { IListing } from '../models/listing.model';
+import { Router } from '@angular/router';
+import { ListingService } from '../listing/listing.service';
 
 @Component({
   selector: 'app-listingdetails',
   templateUrl: './listingdetails.component.html',
   styleUrls: ['./listingdetails.component.css']
 })
-export class ListingdetailsComponent {
+export class HomeComponent {//implements OnInit {
+  listings: IListing[] = [];
 
+  constructor(private _router: Router, private _listingService: ListingService) { }
+
+
+  ngOnInit() {
+    this.getListings();
+  }
+
+
+  //Method to get the all listings:
+
+  getListings(): void {
+    this._listingService.getListings()
+      .subscribe(data => {
+        console.log('All', JSON.stringify(data)); //Just for debugging purposes, remember to REMOVE or COMMENT it out. 
+        this.listings = data;
+      });
+  }
 }
