39ee53c094c7eb3367aef5ec4d7b6ab48fa9d989
s364564 <s364564@oslomet.no>
2023-11-28 19:14:22 +0100

    Removed WeatherForecast and dead folders

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,6 +4,12 @@ namespace RentHiveV2.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? ProfilePicture { get; set; }
 
+        //NAV. PROP. 
+        public virtual ICollection<Listing>? Listings { get; set; }
+        public ICollection<Bookings>? Bookings { get; set; }
     }
 }
