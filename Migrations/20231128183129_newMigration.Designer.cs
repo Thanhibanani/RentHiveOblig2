412b1bb0956ce5eacc46e7cc7a80207f04c8227a
s364564 <s364564@oslomet.no>
2023-11-28 22:12:30 +0100

    truncate pipe

--- Migrations/20231128183129_newMigration.Designer.cs
+++ Migrations/20231128183129_newMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RentHiveV2.DAL;
 
@@ -11,9 +12,10 @@ using RentHiveV2.DAL;
 namespace RentHiveV2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231128183129_newMigration")]
+    partial class newMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
