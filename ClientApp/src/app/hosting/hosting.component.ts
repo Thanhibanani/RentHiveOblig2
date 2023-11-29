425808220d4f22681acc83fe6e68953a122dfe31
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2023-11-29 21:37:12 +0100

    Fetch bookings fixed yaerrr

--- ClientApp/src/app/hosting/hosting.component.ts
+++ ClientApp/src/app/hosting/hosting.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:152ef03bc65e9763d75a02b9d8ecaf554e1a6b52b0c5728d8de5b835eb7248a9
-size 2503
+oid sha256:5b2f9463b727298d1cb7155bbae57ee265393313e368b9f440387c223e948e02
+size 5992
--- ClientApp/src/app/userhistory/userhistory.component.ts
+++ ClientApp/src/app/userhistory/userhistory.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3aa2d80489911e6be0998101454ecdafb200dd0d59f50a345b8f8ef7baf76a46
-size 2004
+oid sha256:d0d47e4ca4ca766b4305b354e68a6dd3f4325f836f6bbd3a2438d71aabab22dc
+size 2526
