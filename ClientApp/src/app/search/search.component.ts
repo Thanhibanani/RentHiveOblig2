e24114f307509b494f50ed5e2653f2701245c3a3
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2023-11-30 10:16:48 +0100

    yaer search format

--- ClientApp/src/app/search/search.component.ts
+++ ClientApp/src/app/search/search.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca07bd598c2bf1d28bfc6bca7cf1d525c26e2a37526b1706efc6c509ef2ae5d0
-size 2740
+oid sha256:3f63b3c973dc8a15981f4a007d9778702f7bb9f523a005fed6ad1e10f0342876
+size 2849
