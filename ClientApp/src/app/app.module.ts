727bdd4e60208f58a1fa33e39a853f7c753421ba
s364564 <s364564@oslomet.no>
2023-11-30 08:47:50 +0100

    valuta pipe

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e220db442a00c371b0a8e9575d2ef78fe4769dba0a5d850fdf69344ae8e2b3d7
-size 2604
+oid sha256:4b10ad93d508f44c652f4ca012bed63390dca79cacc55873f60bb94089aebd4a
+size 2673
---
+++ ClientApp/src/app/pipes/valuta.pipe.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e8d9b7bcfaac004beadeb82b07cc8bf543b115f464eb2ad073ee2df4fcf59bd0
+size 546
